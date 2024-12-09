68a9d76c34e1572653ee78eda8a9f6b255f8dffb
Julia Cheperis <julia.cheperis@dataart.com>
2024-12-09 22:55:58 +0100

    add files

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.models import Contact
+from src.database.models import Contact, User
 from src.schemas import ContactBase
 
 from sqlalchemy.sql.expression import and_, extract, or_
@@ -17,6 +17,7 @@ class ContactRepository:
 
     async def get_contacts(
         self,
+        user: User,
         skip: int = 0,
         limit: int = 10,
         first_name: Optional[str] = None,
@@ -33,27 +34,33 @@ class ContactRepository:
             filters.append(Contact.email.like(f"%{email}%"))
 
         # Build the query dynamically
-        stmt = select(Contact).where(and_(*filters)).offset(skip).limit(limit)
+        stmt = (
+            select(Contact)
+            .filter_by(user=user)
+            .where(and_(*filters))
+            .offset(skip)
+            .limit(limit)
+        )
 
         contacts = await self.db.execute(stmt)
         return contacts.scalars().all()
 
-    async def get_contact_by_id(self, contact_id: int) -> Contact | None:
-        stmt = select(Contact).filter_by(id=contact_id)
+    async def get_contact_by_id(self, contact_id: int, user: User) -> Contact | None:
+        stmt = select(Contact).filter_by(id=contact_id, user=user)
         contact = await self.db.execute(stmt)
         return contact.scalar_one_or_none()
 
-    async def create_contact(self, body: ContactBase) -> Contact:
-        contact = Contact(**body.model_dump(exclude_unset=True))
+    async def create_contact(self, body: ContactBase, user: User) -> Contact:
+        contact = Contact(**body.model_dump(exclude_unset=True), user=user)
         self.db.add(contact)
         await self.db.commit()
         await self.db.refresh(contact)
         return contact
 
     async def update_contact(
-        self, contact_id: int, body: ContactBase
+        self, contact_id: int, body: ContactBase, user: User
     ) -> Contact | None:
-        contact = await self.get_contact_by_id(contact_id)
+        contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             contact.first_name = body.first_name
             contact.last_name = body.last_name
@@ -66,33 +73,37 @@ class ContactRepository:
             await self.db.refresh(contact)
         return contact
 
-    async def remove_contact(self, contact_id: int) -> Contact | None:
-        contact = await self.get_contact_by_id(contact_id)
+    async def remove_contact(self, contact_id: int, user: User) -> Contact | None:
+        contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             await self.db.delete(contact)
             await self.db.commit()
         return contact
 
-    async def get_upcoming_birthdays(self, days: int = 7) -> List:
+    async def get_upcoming_birthdays(self, user: User, days: int = 7) -> List:
         today = date.today()
         target_date = today + timedelta(days=days)
 
         if target_date.year == today.year:
             # Query stays within the same year
-            stmt = select(Contact).filter(
-                or_(
-                    and_(
-                        extract("month", Contact.birthday) == today.month,
-                        extract("day", Contact.birthday) >= today.day,
-                    ),
-                    and_(
-                        extract("month", Contact.birthday) == target_date.month,
-                        extract("day", Contact.birthday) <= target_date.day,
-                    ),
-                    and_(
-                        extract("month", Contact.birthday) > today.month,
-                        extract("month", Contact.birthday) < target_date.month,
-                    ),
+            stmt = (
+                select(Contact)
+                .filter_by(user=user)
+                .filter(
+                    or_(
+                        and_(
+                            extract("month", Contact.birthday) == today.month,
+                            extract("day", Contact.birthday) >= today.day,
+                        ),
+                        and_(
+                            extract("month", Contact.birthday) == target_date.month,
+                            extract("day", Contact.birthday) <= target_date.day,
+                        ),
+                        and_(
+                            extract("month", Contact.birthday) > today.month,
+                            extract("month", Contact.birthday) < target_date.month,
+                        ),
+                    )
                 )
             )
         else:
--- src/schemas.py
+++ src/schemas.py
@@ -25,3 +25,27 @@ class ContactResponse(ContactBase):
     id: int
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str
+    avatar: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class RequestEmail(BaseModel):
+    email: EmailStr
